95e30ac1e8ca2f500caf026d342c04f3959bf818
opengrid <lech.wilczynski@gmail.com>
2011-08-19 12:03:46 +0000

    Problem 3 - MaxPrimeFactor

--- euler.js
+++ euler.js
@@ -1,5 +1,16 @@
 var _u = require("./underscore.js");
 var Euler = (function() {
+    var primefactors = function(number) {
+        var factors = [1], n = number;
+        for(var i = 2; i <= n; i++) {
+            while(n % i === 0) {
+                factors.push(i);
+                n /= i;
+            }
+        }
+        return factors;
+    };
+    
     var problems = {
         1: {
             'link': "http://projecteuler.net/index.php?section=problems&id=1",
@@ -25,6 +36,14 @@ var Euler = (function() {
                 };
                 return efib(4e6);
             }
+        },
+        3: {
+            'link': 'http://projecteuler.net/index.php?section=problems&id=3',
+            'desc': "What is the largest prime factor of the number 600851475143",
+            solve: function() {
+                var n = 600851475143;                
+                return _u.max(primefactors(n));                
+            }
         }
     };
     return {
@@ -34,5 +53,7 @@ var Euler = (function() {
         }
     };
 })();
+
 console.log(Euler.result(1));
 console.log(Euler.result(2));
+console.log(Euler.result(3));
